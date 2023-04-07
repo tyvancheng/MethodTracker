80f39e927ed88dd0af81188e9ce0e8a594a125f7
Tyvan Cheng <tyvancheng@gmail.com>
2023-04-06 20:34:12 -0400

    cleaned up comments, able to accept functions with no classes and before a class

--- ruby_tracker.rb
+++ ruby_tracker.rb
@@ -1,115 +1,10 @@
 
-# Dir.glob('*.rb') do |file|
-#   next if file == "ruby_tracker.rb"
-#   puts "-------------------------"
-#   puts "Methods in #{file}:"
-#   puts
-#   File.open(file, 'r') do |f|
-#     class_name = ""
-#     f.each_line do |line|
-#       if line =~ /^ *class ([a-zA-Z0-9_?!=]+)(.*)$/
-#         method_name = $1
-#         class_name = method_name
-#           # puts " #{method_name}:"
-#           puts line
-#           puts
-#       elsif line =~ /^ *def self.([a-zA-Z0-9_?!=]+)(.*)$/
-#         method_name = $1
-#         class_name.split.length > 1? class_name = class_name[0] : class_name
-#             puts "  #{class_name}::#{method_name}:"
-#             puts line
-#             puts
-#       elsif line =~ /^ *def ([a-zA-Z0-9_?!=]+)(.*)$/
-#           method_name = $1
-#           class_name.split.length > 1? class_name = class_name[0] : class_name
-#             puts "  #{method_name}:"
-#             puts line
-#             puts
-#       end
-#     end
-#   end
-# end
-
-# Dir.glob('*.rb') do |file|
-#   next if file == "ruby_tracker.rb"
-#   puts "-------------------------"
-#   puts "Methods in #{file}:"
-#   printf("%-20s %-20s %s\n", "Class", "Method", "Definition")
-#   puts "-"*60
-#   File.open(file, 'r') do |f|
-#     class_name = ""
-#     f.each_line do |line|
-#       if line =~ /^ *class ([a-zA-Z0-9_?!=]+)(.*)$/
-#         method_name = $1
-#         class_name = method_name
-#         printf("%-20s %-20s %s", class_name, "", line)
-#       elsif line =~ /^ *def self.([a-zA-Z0-9_?!=]+)(.*)$/
-#         method_name = $1
-#         class_name.split.length > 1? class_name = class_name[0] : class_name
-#         printf("%-20s %-20s %s", class_name, method_name, line)
-#       elsif line =~ /^ *def ([a-zA-Z0-9_?!=]+)(.*)$/
-#         method_name = $1
-#         class_name.split.length > 1? class_name = class_name[0] : class_name
-#         printf("%-20s %-20s %s", class_name, method_name, line)
-#       end
-#     end
-#   end
-# end
-# def table_lines
-#   puts "| %-20s | %-20s | %-35s |" % ["Type", "Method", "Arguments"]
-#   puts "|-#{"-"*20}-|-#{"-"*20}-|-#{"-"*35}-|"
-# end
-
-# Dir.glob('*.rb') do |file|
-#   next if file == "ruby_tracker.rb"
-#   puts
-#   puts "\e[31m" + "-"*60 + "\e[0m"
-#   puts "Methods in #{file}:"
-#   puts
-#   File.open(file, 'r') do |f|
-#     classless = true
-#     class_name = ""
-#     f.each_line do |line|
-#       if line =~ /^ *class ([a-zA-Z0-9_?!=]+)(.*)$/
-#         classless = false
-#         class_name = $1
-#         puts "\e[1m#{line}\e[0m"
-#         table_lines
-#       elsif line =~ /^ *def self\.([a-zA-Z0-9_?!=\[\]]+)(.*)\s*(\((.*)\))?$/
-#         if classless
-#           table_lines
-#           classless = false
-#         end
-#         method_name = $1
-#         arguments = $2
-#         puts "| %-20s | %-20s | %-35s |" % ["Class", method_name, arguments]
-#       elsif line =~ /^ *def ([a-zA-Z0-9_?!=\[\]]+)(.*)\s*(\((.*)\))?$/
-#         if classless
-#           table_lines
-#           classless = false
-#         end
-#         method_name = $1
-#         arguments = $2
-#         puts "| %-20s | %-20s | %-35s |" % ["Instance", method_name, arguments]
-#       end
-#     end
-#   end
-# end
-
-# def table_lines
-#   printf("%-20s %-20s %-20s\n", "Type", "Method", "Arguments")
-#   puts "-"*60
-# end
-
-
-
 def table_lines(rows)
-  # puts rows
-  columns = ["Type", "Method", "Arguments"] ###throws error
+
+  columns = ["Type", "Method", "Arguments"] #Header
   max = rows.flatten.inject {|a,e| e.length > a.length ? a=e : a}.length
-  max < 10 ? max = 10 : max = max
+  max < 10 ? max = 10 : max = max #Defines max length per table
   column_widths = [8,max,max]
-  # puts max
   # Print header row
   puts  "| #{columns.map.with_index { |c, i| i==0 ? c.ljust(8) :  c.ljust(max) }.join(" | ")} |"
 
@@ -135,28 +30,26 @@ Dir.glob('*.rb') do |file|
         next
       end
       if line =~ /^ *class ([a-zA-Z0-9_?!=<>\s]+)(.*)$/
+        if !rows.empty?
+          table_lines(rows) 
+          rows = []
+        end
         classless = false
         class_name = $1
         puts "\n\e[1m#{line}\e[0m"
       elsif line =~ /^ *module ([a-zA-Z0-9_?!=<>\s]+)(.*)$/
+        if !rows.empty?
+          table_lines(rows) 
+          rows = []
+        end
         classless = false
         class_name = $1
         puts "\n\e[1m#{line}\e[0m"
       elsif line =~ /^ *def self\.([a-zA-Z0-9_?!=\[\]]+)(.*)\s*(\((.*)\))? *$/
-        # if classless
-        #   table_lines(rows)
-        #   rows = []
-        #   classless = false
-        # end
         method_name = $1
         arguments = $2
         rows << ['Class', method_name, arguments]
       elsif line =~ /^ *def ([a-zA-Z0-9_?!=\[\]]+)(.*)\s*(\((.*)\))? *$/
-        # if classless
-        #   table_lines(rows)
-        #   rows = []
-        #   classless = false
-        # end
         method_name = $1
         arguments = $2
         rows << ['Instance', method_name, arguments]
